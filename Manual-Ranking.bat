895c8202f069ccb6ebaa36874c8cd0d6e3c6b5b4
Harry Clark <53331024+DrFuff@users.noreply.github.com>
2020-07-30 21:54:26 +1000

    v0.5
    Added code that actually runs mugen, fixed some text issues when printed the "VS" information and added "forever-mode" bool which runs the code without input from the user.  Fun fact: if i didn't change the year on the version date these changes would have been added 2 days later than the last :)

--- Manual-Ranking.bat
+++ Manual-Ranking.bat
@@ -1,7 +1,7 @@
 @echo off
-Title Manual Ranker for Stuff by Dr.Fuff v0.4
+Title Manual Ranker for Stuff by Dr.Fuff v0.5
 Color 1e
-:: version date = 28/07/2019  ;DD/MM/YYYY
+:: version date = 30/07/2020  ;DD/MM/YYYY
 :: command for ranking chars using select.def
 :: made for games with tons of chars or people wishing to rank a collection
 
@@ -12,14 +12,17 @@ Color 1e
 
 :: USER VARIABLES
 
+:: your system.def file path
+SET system=bM1point0/brokenMUGEN/def/systemEDIT.def
+
 :: your select.def file path (usually data/select.def)
-SET select=./brokenSELECT.def
+SET select=./data/bM1point0/brokenSELECT.def
 
 :: set character you wish to rank
 SET rankie=kfm
 
 :: line number of first character for selection range
-SET min=232
+SET min=1
 
 :: line number of last character for selection range
 SET max=1
@@ -29,6 +32,9 @@ SET ai=1
 
 :: number of rounds
 SET rounds=3
+
+:: loops with or without prompt (1=auto play, 0=waits for input after match finishes)
+SET forever-mode=1
 
 :: Stage for ranking (include ".def" at the end, "randomselect" if you want random stages)
 SET stage=randomselect
@@ -96,7 +102,7 @@ del almostdone.txt
 goto :get-char
 ::------------- END SELECT CONVERSION ------------
 
-
+::debug
 :skip-char-read
 for /f "tokens=*" %%E in (allchars.txt) do (set /A charcount = charcount + 1)
 Echo Total Characters = %charcount%
@@ -135,6 +141,11 @@ if "%catagory1%" == "%catagorytemp%" (
     set /a searchC= searchC -1
     goto :catagory1-repeat
 )
+::remove leading spaces
+set catagory1=%catagory1%##
+set catagory1=%catagory1:  ##=##%
+set catagory1=%catagory1: ##=##%
+set catagory1=%catagory1:##=%
 
 ::get the character's author
 ::echo at author
@@ -161,6 +172,11 @@ if "%author1%" == "%authortemp%" (
     set /a searchA= searchA -1
     goto :author1-repeat
 )
+::removes following spaces
+set author1=%author1%##
+set author1=%author1:  ##=##%
+set author1=%author1: ##=##%
+set author1=%author1:##=%
 
 :skip-comments
 ::get the random character's rank
@@ -170,11 +186,8 @@ for /f "skip=%skip% delims=, tokens=3" %%I in (allchars.txt) do (
 )
 :rank1-result
 set rank1=%rank1:*:=%
-set rank1=%rank1:~1,8%
-set isTen=%rank1:10=%
-if NOT "%isTen%"=="%rank1%" goto :dont-shorten
-set rank1=%rank1:~0,7%
-:dont-shorten
+set rank1=%rank1:order=%
+for /f %%a in ("%rank1%") do set rank1=%%a
 
 ::get the random character's real name (kinda)
 for /f "delims=/ tokens=2" %%I in ("%char1%") do (
@@ -184,9 +197,12 @@ for /f "delims=/ tokens=2" %%I in ("%char1%") do (
 :get-name1
 set name1=%name1:*:=%
 
+set command=mugen -r %system% -rounds %rounds% -p1.ai %ai% "%char1%" -p2.ai 1 "%rankie%" -s %stage:.def=%
+
+::what shows up before the game runs the command
 CLS
 echo.
-echo. %name1% [%catagory1%] by %author1% [%rank1%]
+echo. %name1% [%catagory1%] by %author1% [order%rank1%]
 echo.                   -==V.S==-
 echo. %rankie% 
 echo.                   -=STAGE=-
@@ -194,10 +210,14 @@ echo.                   %stage:.def=%
 echo.
 echo the line number for the character was %targetchar%
 echo game would start with:
-echo mugen -r %select% -rounds %rounds% -p1.ai %ai% %char1% -p2.ai 1 %rankie% -s stages/%stage%
+::echo mugen -r %select% -rounds %rounds% -p1.ai %ai% %char1% -p2.ai 1 %rankie% -s stages/%stage%
+echo %command%
 echo.
-echo use ctrl+c to exit, or
+%command%
 
+::stops the madness or not depending on settings
+if %forever-mode%==1 goto get-char
+echo use ctrl+c to exit, or
 pause
 
 goto :get-char
